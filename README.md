2444f1534cc0da4d33658546ea51830ddb8d200f
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-06-03 11:43:22 -0700

    Frequency Counter - sameFrequency

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Course 'JavaScript Algorithms and Data Structures Masterclass' Udemy
+https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/
---
+++ SameFrequency.js
@@ -0,0 +1,31 @@
+const sameFrequency = (num1, num2) => {
+    
+    const str1 = String(num1);
+    const str2 = String(num2);
+
+    if(str1.length !== str2.length){
+        return false;
+    }
+    const freq = new Map();
+    for(let i = 0; i < str1.length; i++){
+       if(freq.has(str1[i])){
+        freq.set(str1[i], freq.get(str1[i]) + 1);
+       }else{
+          freq.set(str1[i], 1);
+       }
+    }
+
+    for(let i = 0; i < str2.length; i++){
+        if(!freq.has(str2[i])){
+            return false;
+        }
+    }
+
+    return true;
+    
+}
+
+console.log(sameFrequency(182,281) === true);
+console.log(sameFrequency(34,14) === false); 
+console.log(sameFrequency(3589578, 5879385) === true);
+console.log(sameFrequency(22,222) === false);
