427a4c723ad8e67ac6fa18b01da9ab3d04409f18
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-06-20 15:08:09 -0700

    shift/unshift

--- linkedList/DoublyLinkedList.js
+++ linkedList/DoublyLinkedList.js
@@ -32,6 +32,7 @@ class DoublyLinkedList {
     if (!this.head) {
       return undefined;
     }
+
     const node = this.tail;
 
     if (this.length === 1) {
@@ -46,11 +47,48 @@ class DoublyLinkedList {
     this.length--;
     return node;
   }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    const node = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = node.next;
+      this.head.prev = null;
+      node.next = null;
+    }
+    this.length--;
+    return node;
+  }
+
+  unshift(val){
+    let newNode = new Node(val);
+    if(!this.head){
+        this.head = newNode;
+        this.tail = newNode;
+    }else{
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 let list = new DoublyLinkedList();
 list.push(1);
 list.push(2);
-console.log(list);
-console.log(list.pop());
-console.log(list.pop());
+list.push(3);
+list.push(4);
+list.push(5);
+console.log(list.head);
+list.shift();
+list.shift();
+console.log(list.head);
