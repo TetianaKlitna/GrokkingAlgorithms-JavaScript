be09bf766fc1b24c4f4a455e20aaa236274e414f
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-06-23 12:24:21 -0700

    bst

---
+++ tree/BST.js
@@ -0,0 +1,54 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let node = new Node(value);
+
+    if (!this.root) {
+      this.root = node;
+      return this;
+    }
+
+    let curr = this.root;
+    while (curr) {
+      if (node.value < curr.value) {
+        if (!curr.left) {
+          curr.left = node;
+          return this;
+        } else {
+          curr = curr.left;
+        }
+      } else if (curr.value < node.value) {
+        if (!curr.right) {
+          curr.right = node;
+          return this;
+        } else {
+          curr = curr.right;
+        }
+      } else {
+        return this;
+      }
+    }
+    return this;
+  }
+}
+
+let bst = new BST();
+bst.insert(5);
+bst.insert(10);
+bst.insert(15);
+bst.insert(6);
+console.log(bst.root.value);
+console.log(bst.root.right.value);
+console.log(bst.root.right.right.value);
+console.log(bst.root.right.left.value);
