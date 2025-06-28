73ff973d54afde9b74937cc9d930ad5138ae90b1
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-06-28 14:15:44 -0700

    MaxBinaryHeap extractMax

--- heap/MaxHeap.js
+++ heap/MaxHeap.js
@@ -27,8 +27,55 @@ class MaxBinaryHeap {
 
     return this;
   }
+
+  extractMax() {
+
+    if(this.values.length === 0) return undefined;
+
+    if(this.values.length === 1) return this.values.pop();
+
+    [this.values[0], this.values[this.values.length - 1]] = [
+      this.values[this.values.length - 1],
+      this.values[0],
+    ];
+
+    const max = this.values.pop();
+ 
+    let ind = 0;
+    let currInd = 0;
+    while (ind < this.values.length) {
+      const nextInd1 = 2 * ind + 1;
+      const nextInd2 = 2 * ind + 2;
+      if (nextInd1 < this.values.length && nextInd2 < this.values.length) {
+        currInd =
+          this.values[nextInd1] > this.values[nextInd2] ? nextInd1 : nextInd2;
+      } else if (nextInd1 < this.values.length) {
+        currInd = nextInd1;
+      } else if (nextInd2 < this.values.length) {
+        currInd = nextInd2;
+      } else {
+        break;
+      }
+  
+      if (this.values[ind] < this.values[currInd]) {
+        [this.values[ind], this.values[currInd]] = [
+          this.values[currInd],
+          this.values[ind],
+        ];
+      } else {
+        break;
+      }
+
+      ind = currInd;
+    }
+    
+    return max;
+  }
 }
 
 const maxHeap = new MaxBinaryHeap();
-maxHeap.insert(10).insert(20).insert(6).insert(6).insert(21);
+maxHeap.insert(10);
 console.log(maxHeap.values);
+console.log(maxHeap.extractMax());
+console.log(maxHeap.values);
+console.log(maxHeap.extractMax());
