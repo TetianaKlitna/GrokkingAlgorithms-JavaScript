3b8952dcbfed03290b35513aebe1687b9835a654
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-07-01 18:11:57 -0700

    graph addVertex/removeVertex/addEdge/removeEdge

---
+++ graphs/Graph.js
@@ -0,0 +1,60 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+      return true;
+    }
+
+    return false;
+  }
+
+  removeVertex(vertex){
+     
+    if(this.adjacencyList[vertex])
+    {
+     while(this.adjacencyList[vertex].length > 0){
+        const neighbor = this.adjacencyList[vertex].pop();
+        this.removeEdge(neighbor, vertex)
+     }
+    }
+    delete this.adjacencyList[vertex];
+  }
+
+  addEdge(vertex1, vertex2) {
+
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2] && !this.adjacencyList[vertex1].includes(vertex2)) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+      return true;
+    }
+
+    return false;
+  }
+
+  removeEdge(vertex1, vertex2) {
+    
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (item) => item !== vertex2
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (item) => item !== vertex1
+      );
+      return true;
+    }
+
+    return false;
+  }
+}
+
+const graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addEdge('A', 'B');
+console.log(graph.adjacencyList['B']);
+graph.removeEdge('A', 'B');
+console.log(graph.adjacencyList);
